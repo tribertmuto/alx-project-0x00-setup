17a79b569abc29a0248af406ad6289fcdf7f17f3
MURINDABIGWI TRIBERT <tribertmuto@gmail.com>
2025-07-21 01:57:01 +0200

    Update index .tsx

--- alx-project-0x00/interfaces/index .tsx
+++ alx-project-0x00/interfaces/index .tsx
@@ -14,3 +14,11 @@ export interface ButtonProps {
   title: string;
   styles: string; // Tailwind utility classes
 }
+export interface PillProps {
+  title: string;
+}
+
+export interface ButtonProps {
+  title: string;
+  styles: string; // ✅ This line is required
+}
